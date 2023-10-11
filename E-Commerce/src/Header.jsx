09e11136120135e211865f255b557679b16ec2d3
Owi27 <owens.nicolas02@icloud.com>
2023-10-10 23:55:53 -0400

    Added Header.jsx

---
+++ E-Commerce/src/Header.jsx
@@ -0,0 +1,17 @@
+
+
+function Header()
+{
+    <header>
+        <h1 className="header-title">E-Commerce</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+        </nav>
+    </header>
+}
+
+export default Header;
