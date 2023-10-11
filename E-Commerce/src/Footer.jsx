62e96c8e468da71086be282d9e23f3b1ffd00989
Owi27 <owens.nicolas02@icloud.com>
2023-10-11 00:44:49 -0400

    Footer kinda done

---
+++ E-Commerce/src/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+function Footer()
+{
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} E-Commerce</p>
+        </footer>
+    );
+}
+
+export default Footer;
