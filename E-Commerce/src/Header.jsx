3a343ee0f4c00159f4b60f4ff768980de7cb1f4b
Owi27 <owens.nicolas02@icloud.com>
2023-10-11 00:25:04 -0400

    Header done for now

--- E-Commerce/src/Header.jsx
+++ E-Commerce/src/Header.jsx
@@ -1,17 +1,20 @@
-
+import './Header.css'
 
 function Header()
 {
-    <header>
+    return(
+        <header>
         <h1 className="header-title">E-Commerce</h1>
         <nav>
-            <ul>
+            <ul className="nav-links">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Shop</a></li>
                 <li><a href="#">About</a></li>
             </ul>
         </nav>
+        <a href="#" className="checkout"><button>Checkout</button></a>
     </header>
+    );
 }
 
 export default Header;
