2daad5cddf77884d4c8b233467e99d69614fd20f
Owi27 <owens.nicolas02@icloud.com>
2023-10-12 11:36:54 -0400

    Added DbContext service to Program.cs

--- E-CommerceAPI/Program.cs
+++ E-CommerceAPI/Program.cs
@@ -1,3 +1,5 @@
+using E_CommerceAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace E_CommerceAPI
 {
@@ -8,6 +10,7 @@ namespace E_CommerceAPI
             var builder = WebApplication.CreateBuilder(args);
             {
                 // Add services to the container.
+                builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
                 builder.Services.AddControllers();
                 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
                 builder.Services.AddEndpointsApiExplorer();
