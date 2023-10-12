f4087db11dfd6ba0ed3abb73bededd13a565f193
Owi27 <owens.nicolas02@icloud.com>
2023-10-12 00:04:33 -0400

    Added proj to solution

---
+++ E-CommerceAPI/Models/Product.cs
@@ -0,0 +1,8 @@
+public class Product
+{
+    public int ProductID { get; set; }
+    public string ProductImage { get; set; }
+    public string ProductName { get; set; }
+    public string ProductDescription { get; set; }
+    public int Price { get; set; }
+}
