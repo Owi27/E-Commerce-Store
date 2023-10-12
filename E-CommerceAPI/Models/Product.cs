c8ec4023571b737da1956b7539895ecd9ac91534
Owi27 <owens.nicolas02@icloud.com>
2023-10-12 00:14:34 -0400

    Fixed .NET Project

--- E-CommerceAPI/Models/Product.cs
+++
@@ -1,8 +0,0 @@
-public class Product
-{
-    public int ProductID { get; set; }
-    public string ProductImage { get; set; }
-    public string ProductName { get; set; }
-    public string ProductDescription { get; set; }
-    public int Price { get; set; }
-}
