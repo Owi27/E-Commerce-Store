9be9745945a6d6215584d15e259524d527909671
Owi27 <owens.nicolas02@icloud.com>
2023-10-12 11:10:11 -0400

    Product Model

---
+++ E-CommerceAPI/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace E_CommerceAPI.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public List<string> ProductImages { get; set; }
+        public string ProductName { get; set; }
+        public string? ProductDescription { get; set; }
+        public int Price { get; set; }
+    }
+}
