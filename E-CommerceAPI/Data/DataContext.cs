246d4cab987fd44b28847d5d3d620276b2f42159
Owi27 <owens.nicolas02@icloud.com>
2023-10-12 11:18:18 -0400

    EF DbSet setup

---
+++ E-CommerceAPI/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using E_CommerceAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_CommerceAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
