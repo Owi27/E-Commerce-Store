2a4916c5b404ec6606837c69e4a704c8dbe00c91
Owi27 <owens.nicolas02@icloud.com>
2023-10-14 02:21:12 -0400

    Added EF Migration

---
+++ E-CommerceAPI/Migrations/20231014061747_InitialCreate.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_CommerceAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ImageURI",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Uri = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ImageURI", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_ImageURI_Products_ProductID",
+                        column: x => x.ProductID,
+                        principalTable: "Products",
+                        principalColumn: "ProductID");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImageURI_ProductID",
+                table: "ImageURI",
+                column: "ProductID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ImageURI");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
--- E-CommerceAPI/Models/Product.cs
+++ E-CommerceAPI/Models/Product.cs
@@ -1,11 +1,22 @@
-﻿namespace E_CommerceAPI.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_CommerceAPI.Models
 {
     public class Product
     {
+        [Key]
         public int ProductID { get; set; }
-        public List<string> ProductImages { get; set; }
+        public List<ImageURI> ProductImages { get; set; } = new List<ImageURI>();
         public string ProductName { get; set; }
         public string? ProductDescription { get; set; }
         public int Price { get; set; }
+    }
+
+    public class ImageURI
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Uri { get; set; } = null;
     }
 }
