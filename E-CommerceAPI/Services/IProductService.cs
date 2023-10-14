952d6a47a9fdd2e173adbf11b1190d3b54f06ce7
Owi27 <owens.nicolas02@icloud.com>
2023-10-14 02:39:15 -0400

    Added Product Service

---
+++ E-CommerceAPI/Services/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using E_CommerceAPI.Models;
+
+namespace E_CommerceAPI.Services
+{
+    public interface IProductService
+    {
+        List<Product> GetAllProducts();
+        Product GetProductById(int id);
+        List<Product> AddProduct(Product newProduct);
+    }
+}
---
+++ E-CommerceAPI/Services/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using E_CommerceAPI.Models;
+
+namespace E_CommerceAPI.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly List<Product> _products = new List<Product>();
+        public List<Product> AddProduct(Product newProduct)
+        {
+           _products.Add(newProduct);
+            return _products;
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return _products;
+        }
+
+        public Product GetProductById(int id)
+        {
+            return _products.FirstOrDefault(p => p.ProductID == id);
+        }
+    }
+}
