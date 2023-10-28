7552fd68c9dfdd3693bf4aeec4c6401333799478
Owi27 <owens.nicolas02@icloud.com>
2023-10-28 17:43:59 -0400

    Added MailKit and cleanedup user controller

--- E-CommerceAPI/Controllers/UserController.cs
+++ E-CommerceAPI/Controllers/UserController.cs
@@ -17,33 +17,33 @@ namespace E_CommerceAPI.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult> Register(UserRegisterRequest request)
+        public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterRequest request)
         {
             return Ok(await _userService.Register(request));
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult> Login(UserLoginRequest request)
+        public async Task<ActionResult<ServiceResponse<string>>> Login(UserLoginRequest request)
         {
             return Ok(await _userService.Login(request));
         }
 
         [HttpPost("verify")]
-        public async Task<ActionResult> Verify(string token)
+        public async Task<ActionResult<ServiceResponse<string>>> Verify(string token)
         {
             return Ok(await _userService.Verify(token));
         }
 
         [HttpPost("forgot-password")]
-        public async Task<ActionResult> ForgotPassword(string email)
+        public async Task<ActionResult<ServiceResponse<string>>> ForgotPassword(string email)
         {
             return Ok(await _userService.ForgotPassword(email));
         }
 
         [HttpPost("reset-password")]
-        public async Task<ActionResult> ResetPassword(string email)
+        public async Task<ActionResult<ServiceResponse<string>>> ResetPassword(ResetPasswordRequest request)
         {
-            return Ok(await _userService.ResetPassword(email));
+            return Ok(await _userService.ResetPassword(request));
         }
     }
 }
